f83ccca71c7e32efcfe72fda006dc1dc70cb37a4
Alex <alex.mani@student.moringaschool.com>
2025-03-14 22:59:21 +0300

    Committed

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
-// Remove the <main> element with id="main"
+
 const main = document.getElementById("main");
 if (main) {
     main.remove();
 }
 
-// Create a new <h1> element
+
 const newHeader = document.createElement("h1");
 
-// Set its id to "victory"
+
 newHeader.id = "victory";
 
-// Add text content with your name
-newHeader.textContent = "YOUR-NAME is the champion"; // Replace YOUR-NAME
 
-// Append it to the body
-document.body.appendChild(newHeader);
+newHeader.textContent = "YOUR-NAME is the champion"; 
+
+
 
